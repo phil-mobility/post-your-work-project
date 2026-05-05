843aa1e01e7ec178492acce7e84aee9d9aff3424
phil-mobility <philipp.oberfrank@cariad.technology>
2026-05-05 15:25:37 +0200

    Added 'valid_months' as variable

--- bikeshare.py
+++ bikeshare.py
@@ -23,6 +23,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     valid_cities = ['chicago', 'new york city', 'washington']
+    valid_months = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
 
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
@@ -34,7 +35,7 @@ def get_filters():
     # get user input for month (all, january, february, ... , june)
     month = get_valid_input(
         "Enter a month (January to June) or 'all': ",
-        ['all', 'january', 'february', 'march', 'april', 'may', 'june']
+        valid_months
     )
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
